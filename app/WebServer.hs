7c2267281214f9af20e1e78d8509b00b4e70c11c
Douglas Burke <dburke.gw@gmail.com>
2014-05-12 09:42:01 -0400

    Set up for running on Heroku

--- app/WebServer.hs
+++ app/WebServer.hs
@@ -2,35 +2,55 @@
 
 -- | A test webserver.
 -- 
-
+-- Try the Heroku Haskell buildpack from
+--
 module Main where
 
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
+import Control.Monad (liftM)
+
+import Data.Default (def)
 import Data.Monoid ((<>))
 
 import Network.HTTP.Types (StdMethod(HEAD))
-import Network.Wai.Middleware.RequestLogger
+-- import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Handler.Warp (defaultSettings, setPort)
+
+import System.Environment (lookupEnv)
 
 import Text.Blaze.Html.Renderer.Text
 
 import Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $ do
+main = do
+  -- Use presence of PORT argument to decide between production and
+  -- test environments. The Scotty documentations suggest calling
+  -- setFdCacheDuration on the settings field, to change the value from
+  -- 0, but do not really explain the implications of why it is
+  -- set to 0 in the first place.
+  --
+  mports <- lookupEnv "PORT"
+  let opts = case read `liftM` mports of
+               Just port -> def { verbose = 0
+                                , settings = setPort port defaultSettings }
+               _ -> def
+ 
+  scottyOpts opts $ do
 
-         middleware logStdoutDev
+    -- middleware logStdoutDev
 
-         get "/" $ redirect "/index.html"
-         get "/index.html" $ fromBlaze intro
-         -- TODO: is this correct for HEAD; or should it just 
-         --       set the redirect header?
-         addroute HEAD "/" $ standardResponse >> redirect "/index.html"
-         addroute HEAD "/index.html" standardResponse
+    get "/" $ redirect "/index.html"
+    get "/index.html" $ fromBlaze intro
+    -- TODO: is this correct for HEAD; or should it just 
+    --       set the redirect header?
+    addroute HEAD "/" $ standardResponse >> redirect "/index.html"
+    addroute HEAD "/index.html" standardResponse
 
-         get "/about.html" $ fromBlaze about
-         addroute HEAD "/about.html" standardResponse
+    get "/about.html" $ fromBlaze about
+    addroute HEAD "/about.html" standardResponse
 
 fromBlaze :: H.Html -> ActionM ()
 fromBlaze = html . renderHtml
