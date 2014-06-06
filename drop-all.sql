9a4526b27db71178f4b2ce4706e140545a2ae676
Douglas Burke <dburke.gw@gmail.com>
2014-06-05 21:14:27 -0400

    Use science data from OCAT, including proposal info, rather than from STS.

---
+++ drop-all.sql
@@ -0,0 +1,6 @@
+DROP TABLE "ScheduleItem";
+DROP TABLE "ScienceObs";
+DROP TABLE "NonScienceObs";
+DROP TABLE "Proposal";
+DROP TABLE "List##Tuple2##String#TimeKS#values";
+DROP TABLE "List##Tuple2##String#TimeKS";
