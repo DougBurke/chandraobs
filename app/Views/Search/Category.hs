5a390be1404295981453b138c3806d450cbe33d4
Douglas Burke <dburke.gw@gmail.com>
2016-03-31 17:38:33 -0400

    Add some explanatory text to the category view.

--- app/Views/Search/Category.hs
+++ app/Views/Search/Category.hs
@@ -9,7 +9,7 @@ module Views.Search.Category (indexPage
 
 -- import qualified Prelude as P
 import Prelude (($), Maybe(..), Int, String
-               , compare, fst, length, mapM_, show)
+               , compare, fst, mapM_)
 
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -21,9 +21,10 @@ import Data.Monoid ((<>), mconcat)
 import Text.Blaze.Html5 hiding (map, title)
 import Text.Blaze.Html5.Attributes hiding (title)
 
-import Types (Schedule(..), SimbadType
+import Types (Schedule(..), ObsIdVal(..), SimbadType
              , simbadTypeToDesc)
 import Utils (defaultMeta, skymapMeta, renderFooter, cssLink
+             , abstractLink
              , basicTypeLinkSearch
              , categoryLinkSearch
              , getNumObs
@@ -84,7 +85,57 @@ renderTypes cats =
 
       scats = sortBy (compare `on` fst) cats
   in div $ do
-    p ("There are " <> toHtml (show (length cats)) <> " categories.")
+    p ("The Chandra X-ray Centre has a yearly proposal deadline, "
+       <> "where Astronomers submit their desired targets, their "
+       <> "reasons for making the observations - in other words, "
+       <> "what Science do we think we'll learn from the "
+       <> "data" <> sup "1" <> " - and a technical justification, "
+       <> "in order to justify the observation length and choice "
+       <> "of "
+       <> (a ! href "/about/instruments.html")
+       "Chandra instrumentation"
+       <> ". The Astronomer choses which category best fits her "
+       <> "science case, and submits the proposal; it is this "
+       <> "category that is shown below. There is often a choice "
+       <> "of category, since a proposal can attempt to answer "
+       <> "many questions, so it is only a rough guide to what "
+       <> "Chandra is being asked to study."
+      )
+
+    p ("Once the proposal deadline has passed, the staff at the "
+       <> "Chandra X-ray Center swing into action, setting up the "
+       <> "review panels - inviting experts from around the world "
+       <> "to come to a hotel near the Boston Airport, sit in "
+       <> "window-less rooms for several days, and come up with a "
+       <> "ranked set of proposals. This is not quite the end of the "
+       <> "story, since the proposal list has to go through a final "
+       <> "check before it can finally be released to the community. "
+       <> "The over-subscription rate is high enough that only around "
+       <> "twenty percent of the proposed observations make it through, "
+       <> "so there are often many more unhappy than happy Astronomers "
+       <> "when the results are announced! The abstracts for successful "
+       <> "proposals can be read by following the "
+       <> (a ! href "/proposal/14400832") "proposal link"
+       <> " for an observation, and then selecting the proposal title "
+       <> "- in this example I chose "
+       <> (a ! href (abstractLink (ObsIdVal 14662)))
+       "An X-ray binary candidate with potential extended emission"
+       <> "."
+      )
+
+    (p ! class_ "footnote")
+      (sup "1" <> " Since Chandra data eventually becomes public - "
+       <> "normally a year after the data was taken, but sometimes much "
+       <> "sooner - and most observations contain many X-ray emitting "
+       <> "sources, the data from a proposal can often end up producing "
+       <> "more results from Astronomers using the data for Archival "
+       <> "studies than come from the original proposal. In fact, "
+       <> "Astronomers can ask to analyze archival data as part of the "
+       <> "proposal process."
+       )
+
+    -- hr
+
     table $ do
              thead $ tr $ do
                th "Category"
