9782e14ad854747307448d355ea102fadafeb0b9
Douglas Burke <dburke.gw@gmail.com>
2017-06-28 08:41:42 -0400

    V. minor tweak of labelling for GO

--- lib/Types.hs
+++ lib/Types.hs
@@ -1617,7 +1617,7 @@ instance Parsable PropType where
 toPropTypeLabel :: PropType -> T.Text
 toPropTypeLabel CAL = "Calibration Observation"
 toPropTypeLabel DDT = "Director's Discretionary Time"
-toPropTypeLabel GO = "General Observation"
+toPropTypeLabel GO = "General Observer"
 toPropTypeLabel GTO = "Guaranteed-Time Observation"
 toPropTypeLabel TOO = "Target of Opportunity"
 
