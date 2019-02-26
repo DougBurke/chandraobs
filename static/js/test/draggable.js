25299fdaa05ff22ddf29417ff46f12373d00df78
Douglas Burke <dburke.gw@gmail.com>
2019-02-26 13:08:49 -0500

    Test: improve drag of window
    This fixes the increase in height of the window seen in many cases
    when the status pane is dragged around.
    
    There is still the problem that if you minimise the window, drag
    overlapping the top of the display area, so you can't close it.

--- static/js/test/draggable.js
+++ static/js/test/draggable.js
@@ -26,30 +26,30 @@ const draggable = (function () {
 
 	// If this comes from any range sliders then cancel the panel
 	// drag. See https://stackoverflow.com/a/20819717
-	// Note that for now I have turned off drag support for the
-	// button panel, which means this isn't needed, but left
-	// in (there seemed to be some problem with events still
-	// bubbling through even with the following and did not
-	// want to investigate too much).
 	//
+	// This shouldn't be needed just yet but left as a comment in
+	// case I add a range slider.
+	//
+	/***
 	if (document.activeElement.tagName === "INPUT") {
 	    return false;
 	}
+	***/
 
-        var toMove = event.target.id;
+        const toMove = event.target.id;
         
         // Store the current location of the event, so we can find out
         // how much we have shifted by.
         //
-        var evpos = getEventPos(event);
+        const evpos = getEventPos(event);
         evpos.id = toMove;
         event.dataTransfer.setData(paneMimeType, JSON.stringify(evpos));
 
         // The id is not supposed to contain a space, and we don't, so
         // it's okay to separate with spaces.
         //
-        var store = evpos.x.toString() + " " + evpos.y.toString() +
-            " " + toMove;
+        const store = evpos.x.toString() + " " + evpos.y.toString() +
+              " " + toMove;
         event.dataTransfer.setData("text/plain", store);
 
         // All we want to do is change the position of the div,
@@ -72,7 +72,7 @@ const draggable = (function () {
         //
         var x0, y0, toMove;
         var fallback = true;
-        var types = event.dataTransfer.types;
+        const types = event.dataTransfer.types;
         for (var i=0; i < types.length; i++) {
             if (types[i] === paneMimeType) {
                 fallback = false;
@@ -80,8 +80,8 @@ const draggable = (function () {
             }
         }
         if (fallback) {
-            var store = event.dataTransfer.getData('text/plain');
-            var tags = store.split(" ");
+            const store = event.dataTransfer.getData('text/plain');
+            const tags = store.split(" ");
             if (tags.length !== 3) {
                 console.log("Invalid store data from drop: [" + store + "]");
                 event.preventDefault();  // is this sensible here?
@@ -91,51 +91,43 @@ const draggable = (function () {
             y0 = parseInt(tags[1]);
             toMove = tags[2];
         } else {
-            var json = event.dataTransfer.getData(paneMimeType);
-            var obj = JSON.parse(json);
+            const json = event.dataTransfer.getData(paneMimeType);
+            const obj = JSON.parse(json);
             x0 = obj.x;
             y0 = obj.y;
             toMove = obj.id;
         }
             
-        var evpos = getEventPos(event);
-        var dx = evpos.x - x0;
-        var dy = evpos.y - y0;
+        const evpos = getEventPos(event);
+        const dx = evpos.x - x0;
+        const dy = evpos.y - y0;
 
-        var div = document.getElementById(toMove);
-        var stackbbox = div.getBoundingClientRect();
-        var parentbbox = div.parentElement.getBoundingClientRect();
+	// This is not expected to fail, so do not try to do much
+	// if it does.
+	//
+	const div = document.querySelector('#' + toMove);
+	if (div === null) {
+	    console.log("INTERNAL ERROR: unable to find div I'm dragging!");
+	    return;
+	}
+
+        const stackbbox = div.getBoundingClientRect();
+        const parentbbox = div.parentElement.getBoundingClientRect();
 
         var x1 = div.offsetLeft + dx;
         var y1 = div.offsetTop + dy;
 
-        var xmin = 0, xmax = parentbbox.width - stackbbox.width;
-        var ymin = 0, ymax = parentbbox.height - stackbbox.height;
+        const xmin = 0, xmax = parentbbox.width - stackbbox.width;
+        const ymin = 0, ymax = parentbbox.height - stackbbox.height;
         if (x1 < xmin) { x1 = xmin; } else if (x1 > xmax) { x1 = xmax; }
         if (y1 < ymin) { y1 = ymin; } else if (y1 > ymax) { y1 = ymax; }
 
+	// Since the div was originally placed with respect to the bottom
+	// retain that here.
+	//
         div.style.left = x1.toString() + "px";
-        div.style.top = y1.toString() + "px";
-
-        // For some reason the width could change, so let's be explicit
-        // and also add in the other values. My original thought was that
-        // there was something strange going on because the initial
-        // placement of the stackinfo div uses top/right, but the move
-        // changes top/left. Experiments weren't conclusive.
-        //
-	// Maybe I am not using the correct width/height here?
-	//
-	// I have no idea what the original problem was, but the following
-	// seems to cause problems too.
-	//
-	/***
-        if (toMove === "stackinfo") {
-            div.style.right = (parentbbox.width - x1 - stackbbox.width).toString()
-                + "px";
-            div.style.bottom = (parentbbox.height - y1 - stackbbox.height).toString()
-                + "px";
-        }
-        ***/
+        // div.style.top = y1.toString() + "px";
+        div.style.bottom = (ymax - y1).toString() + "px";
 
         event.preventDefault();
 
