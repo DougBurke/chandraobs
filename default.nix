0b69f6297348d48ba711ed60228a75c2f155ba1b
Douglas Burke <dburke.gw@gmail.com>
2020-11-02 08:04:56 -0500

    Update version
    For some reason I've had to override the groundhog packages to
    pick up 0.11 rather than 0.10. I don't know why this wasn't a
    problem before.

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 { nixpkgs ? import ./nix {}
-, compiler ? "ghc883" }:
+, compiler ? "ghc884" }:
 
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./chandraobs.nix { }
+let
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+    groundhog = self.callCabal2nix "groundhog-0.11.0" {};
+    groundhog-th = self.callCabal2nix "groundhog-th-0.11" {};
+    groundhog-postgresql = self.callCabal2nix "groundhog-postgresql-0.11" {};
+    };
+  };
+in
+  myHaskellPackages.callCabal2nix "chandraobs" (./.) {}
--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { nixpkgs ? import ./nix {}
-, compiler ? "ghc883"
+, compiler ? "ghc884"
 }:
 let
 
